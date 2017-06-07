ebeeb2ee07d34610d0dc27ac2e6b1b128db13a38
jarindr <jarindr23@hotmail.com>
2017-06-08 03:01:38 +0700

    auto complete

--- client/src/booking/EquipmentsSection.js
+++ client/src/booking/EquipmentsSection.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styles from './EquipmentsSection.sass'
 import { Checkbox } from 'antd'
+import EquipmentsSearch from '../components/EquipmentsSearch'
 class EquipmentSection extends Component {
   render () {
     return (
       <div className={styles.container}>
-        <Checkbox>Prophoto</Checkbox>
-        <Checkbox>Broncolor</Checkbox>
-        <
+        <div>
+          <Checkbox>Prophoto</Checkbox>
+          <Checkbox>Broncolor</Checkbox>
+        </div>
+        <div>
+          <EquipmentsSearch />
+        </div>
       </div>
     )
   }
--- client/src/components/EquipmentsSearch.js
+++ client/src/components/EquipmentsSearch.js
@@ -1,24 +1,46 @@
-import { AutoComplete } from 'antd'
+import { AutoComplete, Input, Icon } from 'antd'
 const Option = AutoComplete.Option
+const OptGroup = AutoComplete.OptGroup
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import _ from 'lodash'
 const dataSource = [
   {
     type: 'prophoto',
-    equipment: 'hello'
+    equipment: 'hello1'
   },
   {
     type: 'prophoto',
-    equipment: 'hello'
+    equipment: 'hello2'
   },
   {
     type: 'prophoto',
-    equipment: 'hello'
+    equipment: 'hello3'
   },
+  {
+    type: 'prophoto',
+    equipment: 'hello4'
+  },
+  {
+    type: 'prophoto',
+    equipment: 'hello5'
+  },
+  {
+    type: 'prophoto',
+    equipment: 'hello6'
+  },
+  {
+    type: 'prophoto',
+    equipment: 'hello7'
+  },
+  {
+    type: 'prophoto',
+    equipment: 'hello8'
+  },
+
   {
     type: 'broncolor',
-    equipment: 'potato'
+    equipment: 'potato4'
   }
 ]
 
@@ -28,18 +50,62 @@ class EquipmentSearch extends Component {
     return option.props.equipment
   }
 
-  renderOption = () => {
+  constructor (props) {
+    super(props)
+    this.data = this.groupDataSource()
+  }
 
+  groupDataSource = () => {
+    return _(dataSource)
+    .groupBy(x => x.type)
+    .map((value, key) => {
+      return {
+        type: key,
+        equipments: value
+      }
+    }
+    ).value()
+  }
+
+  renderOption = () => {
+    return this.data.map(group => (
+      <OptGroup
+        key={group.type}
+        label={group.type}
+      >
+        {group.equipments.map(opt => (
+          <Option key={opt.equipment} value={opt.equipment}>
+            {opt.title}
+            <span className='certain-search-item-count'>{opt.equipment}</span>
+          </Option>
+    ))}
+      </OptGroup>
+    )).concat([
+      <Option disabled key='all' className='show-all'>
+        <a
+          href='https://www.google.com/search?q=antd'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          查看所有结果
+        </a>
+      </Option>
+    ])
+  }
+  filterOption = (value, option) => {
+    return new RegExp(value, 'g').test(option.key)
   }
 
   render () {
     return (
       <AutoComplete
-        style={{ width: 200 }}
-        dataSource={dataSource}
-        placeholder='try to type `b`'
+        dataSource={this.renderOption()}
+        size='large'
+        style={{ width: '100%' }}
         filterOption={this.filterOption}
-      />
+      >
+        <Input suffix={<Icon type='search' className='certain-category-icon' />} />
+      </AutoComplete>
     )
   }
 }
